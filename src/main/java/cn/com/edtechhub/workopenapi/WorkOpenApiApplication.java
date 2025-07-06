25b54e2acfb14e7e8fbecf5c4f2af85b2c2ae520
limou3434 <898738804@qq.com>
2025-07-06 15:23:44 +0800

    初始化项目

---
+++ src/main/java/cn/com/edtechhub/workopenapi/WorkOpenApiApplication.java
@@ -0,0 +1,13 @@
+package cn.com.edtechhub.workopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkOpenApiApplication.class, args);
+    }
+
+}
